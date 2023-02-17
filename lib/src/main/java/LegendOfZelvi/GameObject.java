b7efb7f4e68b11c0b7b33ff0d39080121d55e962
Aneeeeesh <asokananeesh8@gmail.com>
2023-02-17 22:46:28 +0530

    gameobject

---
+++ lib/src/main/java/LegendOfZelvi/GameObject.java
@@ -0,0 +1,23 @@
+package LegendOfZelvi;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+public class GameObject {
+	public int x,y;
+	BufferedImage image;
+	Rectangle collider;
+	
+	public void start() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render(Graphics g) {
+		
+	}
+}
